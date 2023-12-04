98cc25dac8ebb0d627759a7d247ed6485c56be66
Alec <alecpierce19@gmail.com>
2023-12-03 22:36:02 -0600

    created animations component

--- test-app/src/app/app.module.ts
+++ test-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { AnimationsComponent } from './animations/animations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AnimationsComponent
   ],
   imports: [
     BrowserModule,
