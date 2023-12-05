3406e45de3c8c2864459bb943ea7d63a46704f37
Alec <alecpierce19@gmail.com>
2023-12-04 22:32:47 -0600

    added basic animations

--- test-app/src/app/animations/animations.component.ts
+++ test-app/src/app/animations/animations.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-animations',
   templateUrl: './animations.component.html',
-  styleUrls: ['./animations.component.sass']
+  styleUrls: ['./animations.component.sass'],
+  animations: [
+    // animation triggers go here
+    trigger('openClose', [
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.8,
+        backgroundColor: 'blue'
+      })),
+      transition('open => closed', [
+        animate('1s ease-in')
+      ]),
+      transition('closed => open', [
+        animate('0.5s ease-out')
+      ]),
+      transition('on => off, off => void', [
+        animate('0.5s ease-out')
+      ])
+    ])
+  ]
 })
 export class AnimationsComponent {
+  isOpen = true;
 
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
 }
